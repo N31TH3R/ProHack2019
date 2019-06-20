7b37ec30ad3599d4afe7cd4bb3981974de7b5261
Teo Adeus <khizhenokm@gmail.com>
2019-06-20 22:33:01 +0300

    Всякое

--- TimeManager/TimeManager/MainWindow.xaml.cs
+++ TimeManager/TimeManager/MainWindow.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TimeManager
 {
@@ -20,14 +13,79 @@ namespace TimeManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Timer timer;
         public MainWindow()
         {
             InitializeComponent();
+            List<WorkItem> workitems = new List<WorkItem>
+            {
+                new WorkItem{Id = 44444, Title = "Kek"},
+                new WorkItem{Id = 65656, Title = "Lol"},
+                new WorkItem{Id = 567567567, Title = "jjjjjjjs"}
+            };
+
+            workItemsList.ItemsSource = workitems;
         }
 
         private void ButtonExit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (timer != null)
+                timer.Close();
+            timer = new Timer(1000);
+            timer.Elapsed += (x, y) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                ((WorkItem)((Button)sender).DataContext).AddTime(TimeSpan.FromMilliseconds(1000)));
+            };
+
+            timer.Start();
+        }
+    }
+
+    public class WorkItem : INotifyPropertyChanged
+    {
+        private int _id;
+        private string _title;
+        private TimeSpan _time;
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string Time => _time.ToString();
+
+        public void AddTime(TimeSpan ts)
+        {
+            _time = _time.Add(ts);
+            NotifyPropertyChanged("Time");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
